ea78adf5456a15a59d3b16e9ca2018ce36448883
Mahfujur51 <overcastmoon@gmail.com>
2020-05-10 12:34:35 +0600

    logout done

--- logout.php
+++ logout.php
@@ -2,12 +2,13 @@
 session_start();
 include("includes/config.php");
 $_SESSION['login']=="";
-date_default_timezone_set('Asia/Kolkata');
+date_default_timezone_set('Asia/Dhaka');
 $ldate=date( 'd-m-Y h:i:s A', time () );
-mysqli_query($con,"UPDATE userlog  SET logout = '$ldate' WHERE userEmail = '".$_SESSION['login']."' ORDER BY id DESC LIMIT 1");
+$useremail=$_SESSION['login'];
+mysqli_query($con,"UPDATE tbl_userlog  SET logoutime = '$ldate' WHERE useremail = '$useremail' ORDER BY id DESC LIMIT 1");
 session_unset();
 $_SESSION['errmsg']="You have successfully logout";
 ?>
 <script language="javascript">
-document.location="index.php";
+	document.location="index.php";
 </script>
